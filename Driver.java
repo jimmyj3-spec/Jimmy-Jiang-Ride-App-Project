a92ea54fb77b3281aa97ff0aa618ff31b64f4eac
jimmyj3-spec <jimmyj3@uci.edu>
2025-12-31 15:55:38 -0800

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,18 @@
+package rideApp;
+
+public class Driver{
+	private String driverName;
+	
+	public Driver(String driverName) {
+		this.driverName = driverName;
+	}
+	
+	public String getDriverName() {
+		return this.driverName;
+	}
+	
+	public String toString() {
+		return driverName;
+	}
+	
+}
